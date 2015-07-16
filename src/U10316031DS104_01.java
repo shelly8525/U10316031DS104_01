11decbfa2f18382971a970c9b8d6c2a57381b46c
shelly8525 <shelly8525@yahoo.com.tw>
2015-07-16 23:23:00 +0800

    frame 做出

---
+++ src/U10316031DS104_01.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.util.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class U10316031DS104_01 extends JFrame implements ActionListener{
+	private JLabel label1 = new JLabel("C統計");
+	private JLabel label2 = new JLabel("輸入檔案");
+	private JLabel label3 = new JLabel("輸入內容:");
+	private JLabel label4 = new JLabel("處理結果:");
+	private JLabel label5 = new JLabel("輸入內容:");
+	
+	private JTextField textField1 = new JTextField();
+	private JTextField textField2 = new JTextField();
+	private JTextField textField3 = new JTextField();
+	private JTextField textField4 = new JTextField();
+	
+	private JButton button1 = new JButton("讀取");
+	private JButton button2 = new JButton("處理");
+	private JButton button3 = new JButton("處理結果存檔");
+	private JButton button4 = new JButton("Clear all");
+	
+	public void actionPerformed(ActionEvent e){
+		
+	}
+	
+	public static void main(String[] args){
+		U10316031DS104_01 frame = new U10316031DS104_01();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
